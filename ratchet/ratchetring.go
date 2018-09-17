8d6f1b5462d38e637e5b54ab5640f2e2843ad4e4
Frank Braun <frank@cryptogroup.net>
2018-09-17 20:01:23 +0000

    ratchet.RatchetRing -> ratchet.Ring (stuttering)

--- ratchet/ratchetring.go
+++ ratchet/ratchetring.go
@@ -1,27 +1,27 @@
 package ratchet
 
-// RatchetRing:
+// Ratchet ring:
 // Three ratchets, one past, one current, one future.
 //
 // CounterPast := ((timeNow - StartDate)/duration)
 // CounterCurrent := ((timeNow - StartDate)/duration)+1
 // CounterFuture := ((timeNow - StartDate)/duration)+2
 
-// RatchetRing contains three ratchets, one current, one past, one future.
-type RatchetRing struct {
+// Ring contains three ratchets, one current, one past, one future.
+type Ring struct {
 	past, current, future *State
 }
 
 // NewRatchetRing returns a new, possibly filled, RatchetRing. pastStep is the expected
 // counter value for the past value. Operates on a copy of ratchet.
-func NewRatchetRing(ratchet *State, currentStep uint64) *RatchetRing {
-	rr := new(RatchetRing)
+func NewRatchetRing(ratchet *State, currentStep uint64) *Ring {
+	rr := new(Ring)
 	rr.set(ratchet, currentStep)
 	return rr
 }
 
 // set ring to given currentStep, using a copy of ratchet.
-func (rr *RatchetRing) set(ratchet *State, currentStep uint64) {
+func (rr *Ring) set(ratchet *State, currentStep uint64) {
 	r := ratchet.Copy()
 	c := r.Counter()
 	if c > currentStep {
@@ -41,7 +41,7 @@ func (rr *RatchetRing) set(ratchet *State, currentStep uint64) {
 }
 
 // StepTo steps to currentStep.
-func (rr *RatchetRing) StepTo(currentStep uint64) {
+func (rr *Ring) StepTo(currentStep uint64) {
 	c := rr.current.Counter()
 	if c == currentStep {
 		return
@@ -54,7 +54,7 @@ func (rr *RatchetRing) StepTo(currentStep uint64) {
 }
 
 // Step executes one step for the ratchets.
-func (rr *RatchetRing) Step() {
+func (rr *Ring) Step() {
 	rr.past = rr.current.Copy()   // current is ALWAYS set.
 	rr.current = rr.future.Copy() // future is ALWAYS set.
 	rr.future.Step()              // continue on future.
@@ -62,18 +62,18 @@ func (rr *RatchetRing) Step() {
 
 // Current returns a copy of the current ratchet state for marshalling.
 // The past state will be lost in marshalling.
-func (rr *RatchetRing) Current() *State {
+func (rr *Ring) Current() *State {
 	return rr.current.Copy()
 }
 
 // CurrentStep returns the counter of the current ratchet.
-func (rr *RatchetRing) CurrentStep() uint64 {
+func (rr *Ring) CurrentStep() uint64 {
 	return rr.current.Counter()
 }
 
 // Find the ratchet state that matches the expected public key and return a copy, or nil
 // if not found.
-func (rr *RatchetRing) Find(expect *[32]byte) *State {
+func (rr *Ring) Find(expect *[32]byte) *State {
 	if rr.current != nil && rr.current.PublicKey == *expect {
 		return rr.current.Copy()
 	}
